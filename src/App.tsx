cddb18169564bd486ff73f1d098c6d3b6bbb34d2
aniket2727 <aniketkadam5950@gmail.com>
2024-06-05 11:01:15 +0530

    add all changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 
 
 import NavbarComponets from './Header/Navbar/Navbarcompontes'
+import LoginComponets from './Login/LoginComponets';
+
+
+const Pagenotfound=()=>{
+  return(
+    <div>
+      <p>page not found</p>
+    </div>
+  )
+}
+
 
 function App() {
   return (
     <div>
          <NavbarComponets/>
+         <BrowserRouter>
+           <Routes>
+            <Route path='/login' element={<LoginComponets/>}/>
+            <Route path='*'  element={<Pagenotfound/>}/>
+           </Routes>
+         </BrowserRouter>
+       
       
      
     </div>
